8b8bc9b9fa2fca981d747a0b91be3d3d41da94d4
nknknjnjnjsgguswhiddv <118340988+nknknjnjnjsgguswhiddv@users.noreply.github.com>
2024-10-01 12:28:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=yB9ExJjD#fWPmVtvoN8qHwN2FuHnC5Jvp1uYZsixqnajR0FO2qgY' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=ackHgLpb#-j6ccPd3KEvdVNVkKL1tK0ayJeDTK1H7pgKaaI14plk' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
